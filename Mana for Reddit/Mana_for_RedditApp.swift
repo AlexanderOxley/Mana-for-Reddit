a25fe33faaac430874cf2845f4440acde68ca703
Alexander Oxley <36739720+AlexanderOxley@users.noreply.github.com>
2026-04-11 15:22:39 +0200

    Initial Commit

---
+++ Mana for Reddit/Mana_for_RedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mana_for_RedditApp.swift
+//  Mana for Reddit
+//
+//  Created by Alexander Oxley on 11.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Mana_for_RedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
